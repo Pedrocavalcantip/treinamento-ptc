9f9f5e5c21e41505f40e833bf8c9beee0bed88a9
Pedro Cavalcanti <pfcc@cin.ufpe.br>
2026-04-28 02:29:48 -0300

    feat: rota de criacao de calcados - post

---
+++ server/src/controllers/CalcadosController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export class CalcadosController {
+  async create(req: Request, res: Response) {
+    
+    try {
+      const { nome_produto, cor, marca, tamanho, preco, quantidade_em_estoque } = req.body;
+
+      if (!nome_produto || !marca || !preco) {
+        return res.status(400).json({ error: "Faltam dados obrigatórios" });
+      }
+      const precoEmCentavos = Math.round(preco * 100);
+
+        const novoCalcado = await prisma.calcado.create({
+            data: {
+                nome_produto,
+                cor,
+                marca,
+                tamanho,
+                preco: precoEmCentavos,
+                quantidade_em_estoque
+            }
+    });
+
+      return res.status(201).json({ 
+        message: "Calçado criado !", 
+        calcado: novoCalcado
+      });
+
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro interno no servidor na criação do calçado" });
+    }
+  }
+
+  
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import {  readAllUsers } from "./controllers/UserController";
-
+import { CalcadosController } from "./controllers/CalcadosController";
 
 const routes = express.Router();
+const calcadosController = new CalcadosController();
 
 routes.get("/users", readAllUsers);
-
+routes.post("/calcados", (req, res) => calcadosController.create(req, res));
 
 export default routes;
